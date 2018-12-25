7769d38c84cb5af918ccbbf11fef0ae5a78617a2
Cristi Raduca <raducapetrecristian@gmail.com>
2018-12-25 13:06:13 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Cristi Raduca on 25/12/2018.
+//  Copyright © 2018 Cristi Raduca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
